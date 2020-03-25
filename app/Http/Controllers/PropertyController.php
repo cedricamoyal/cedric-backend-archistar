77c5a7f7962ae05d6e54b8f16e58c0087d7da202
Cedric Amoyal <cedric.amoyal@gmail.com>
2020-03-25 19:08:38 +1100

    work on Get a summary of all property analytics for an inputted for suburb, state and country

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -42,32 +42,78 @@ class PropertyController extends Controller
         return $propertyWithAnalytics;
     }
 
+    // Get a summary of all property analytics for an inputted suburb
+    // (min value, max value, median value, percentage properties with a value, percentage properties without a value)
     public function suburbAnalytics ($suburb)
     {
+        // Get all the properties for the suburb "$suburb"
         $properties = Property::with('analytics')->where('suburb', $suburb)->get();
 
-        $arraysOfListOfValuesPerSuburbPerType = $this->getListOfValuesPerSuburbPerType($properties);
+        // Get list of values per suburb and per type - See format below:
+        // {
+        //     "max_Bld_Height_m": [36,31,17, ...],
+        //     "min_lot_size_m2": [896,801,1067, ...],
+        //     "fsr": [34, 17, ...],
+        //     "number of properties": 16
+        // }
+        $arraysOfListOfValuesPerSuburbPerType = $this->getListOfValuesPerType($properties);
 
-        $arraysOfMinMaxMedianValues = $this->getSummaryOfAllPropertyAnalytics($arraysOfListOfValuesPerSuburbPerType);
+        // Get suburb analytics
+        // (min value, max value, median value, percentage properties with a value, percentage properties without a value)
+        // See format below:
+        // {
+        //     "max_Bld_Height_m": {
+        //         "min value": 10,
+        //         "max value": 36,
+        //         "median value": 18,
+        //         "percentage properties with a value": 100,
+        //         "percentage properties without a value": 0
+        //     },
+        //     "min_lot_size_m2": {
+        //         "min value": 238,
+        //         "max value": 1067,
+        //         "median value": 896,
+        //         "percentage properties with a value": 68.75,
+        //         "percentage properties without a value": 31.25
+        //     },
+        //     "fsr": {
+        //         "min value": 0,
+        //         "max value": 0,
+        //         "median value": 0,
+        //         "percentage properties with a value": "No data",
+        //         "percentage properties without a value": "No data"
+        //     }
+        // }
+        $arrayOfSuburbAnalytics = $this->getSummaryOfAllPropertyAnalytics($arraysOfListOfValuesPerSuburbPerType);
 
-        return $arraysOfMinMaxMedianValues;
+        return $arrayOfSuburbAnalytics;
     }
 
+    // Get a summary of all property analytics for an inputted state
+    // (min value, max value, median value, percentage properties with a value, percentage properties without a value)
     public function stateAnalytics ($state)
     {
+        // Similar to public function suburbAnalytics ($suburb){} - see comments above
         $properties = Property::with('analytics')->where('state', $state)->get();
+        $arraysOfListOfValuesPerStatePerType = $this->getListOfValuesPerType($properties);
+        $arrayOfStateAnalytics = $this->getSummaryOfAllPropertyAnalytics($arraysOfListOfValuesPerStatePerType);
 
-        return $properties;
+        return $arrayOfStateAnalytics;
     }
 
+    // Get a summary of all property analytics for an inputted country
+    // (min value, max value, median value, percentage properties with a value, percentage properties without a value)
     public function countryAnalytics ($country)
     {
+        // Similar to public function suburbAnalytics ($suburb){} - see comments above
         $properties = Property::with('analytics')->where('country', $country)->get();
+        $arraysOfListOfValuesPerCountryPerType = $this->getListOfValuesPerType($properties);
+        $arrayOfCountryAnalytics = $this->getSummaryOfAllPropertyAnalytics($arraysOfListOfValuesPerCountryPerType);
 
-        return $properties;
+        return $arrayOfCountryAnalytics;
     }
 
-    public function getListOfValuesPerSuburbPerType ($array)
+    public function getListOfValuesPerType ($array)
     {
         $data = array(
             "max_Bld_Height_m" => array(),
@@ -98,27 +144,27 @@ class PropertyController extends Controller
                 "max value" => count($array["max_Bld_Height_m"]) ? max($array["max_Bld_Height_m"]) : 0,
                 "median value" => count($array["max_Bld_Height_m"]) ? $this->median($array["max_Bld_Height_m"]) : 0,
                 "percentage properties with a value" =>
-                    (count($array["max_Bld_Height_m"]) && $array["number of properties"]) ? count($array["max_Bld_Height_m"])/$array["number of properties"]*100 : "No data",
+                    (count($array["max_Bld_Height_m"]) && $array["number of properties"]) ? round(count($array["max_Bld_Height_m"])/$array["number of properties"]*100, 2) : "No data",
                 "percentage properties without a value" =>
-                    (count($array["max_Bld_Height_m"]) && $array["number of properties"]) ? 100 - (count($array["max_Bld_Height_m"])/$array["number of properties"]*100) : "No data",
+                    (count($array["max_Bld_Height_m"]) && $array["number of properties"]) ? round(100 - (count($array["max_Bld_Height_m"])/$array["number of properties"]*100),2) : "No data",
             ),
             "min_lot_size_m2" => array(
                 "min value" => count($array["min_lot_size_m2"]) ? min($array["min_lot_size_m2"]) : 0,
                 "max value" => count($array["min_lot_size_m2"]) ? max($array["min_lot_size_m2"]) : 0,
                 "median value" => count($array["min_lot_size_m2"]) ? $this->median($array["min_lot_size_m2"]) : 0,
                 "percentage properties with a value" =>
-                    (count($array["min_lot_size_m2"]) && $array["number of properties"]) ? count($array["min_lot_size_m2"])/$array["number of properties"]*100 : "No data",
+                    (count($array["min_lot_size_m2"]) && $array["number of properties"]) ? round(count($array["min_lot_size_m2"])/$array["number of properties"]*100, 2) : "No data",
                 "percentage properties without a value" =>
-                    (count($array["min_lot_size_m2"]) && $array["number of properties"]) ? 100 - (count($array["min_lot_size_m2"])/$array["number of properties"]*100) : "No data",
+                    (count($array["min_lot_size_m2"]) && $array["number of properties"]) ? round(100 - (count($array["min_lot_size_m2"])/$array["number of properties"]*100), 2) : "No data",
             ),
             "fsr" => array(
                 "min value" => count($array["fsr"]) ? min($array["fsr"]) : 0,
                 "max value" => count($array["fsr"]) ? max($array["fsr"]) : 0,
                 "median value" => count($array["fsr"]) ? $this->median($array["fsr"]) : 0,
                 "percentage properties with a value" =>
-                    (count($array["fsr"]) && $array["number of properties"]) ? count($array["fsr"])/$array["number of properties"]*100 : "No data",
+                    (count($array["fsr"]) && $array["number of properties"]) ? round(count($array["fsr"])/$array["number of properties"]*100, 2) : "No data",
                 "percentage properties without a value" =>
-                    (count($array["fsr"]) && $array["number of properties"]) ? 100 - (count($array["fsr"])/$array["number of properties"]*100) : "No data",
+                    (count($array["fsr"]) && $array["number of properties"]) ? round(100 - (count($array["fsr"])/$array["number of properties"]*100), 2) : "No data",
             )
         );
 
